1d3e2432f60df22130f901f1b266198813417985
mackfire_me <mackfire_me@hotmail.com>
2018-04-11 23:42:21 +0800

    add new sprites proj

---
+++ Dx11Tut01-Sprites/Game.h
@@ -0,0 +1,69 @@
+//
+// Game.h
+//
+
+#pragma once
+
+#include "StepTimer.h"
+
+using Microsoft::WRL::ComPtr;
+
+// A basic game implementation that creates a D3D11 device and
+// provides a game loop.
+class Game
+{
+public:
+
+	Game();
+
+	// Initialization and management
+	void Initialize(HWND window, int width, int height);
+
+	// Basic game loop
+	void Tick();
+
+	// Messages
+	void OnActivated();
+	void OnDeactivated();
+	void OnSuspending();
+	void OnResuming();
+	void OnWindowSizeChanged(int width, int height);
+
+	// Properties
+	void GetDefaultSize(int& width, int& height) const;
+
+private:
+
+	void Update(DX::StepTimer const& timer);
+	void Render();
+
+	void Clear();
+	void Present();
+
+	void CreateDevice();
+	void CreateResources();
+
+	void OnDeviceLost();
+
+	// Device resources.
+	HWND                                            m_window;
+	int                                             m_outputWidth;
+	int                                             m_outputHeight;
+
+	D3D_FEATURE_LEVEL                               m_featureLevel;
+	ComPtr<ID3D11Device1>           m_d3dDevice;
+	ComPtr<ID3D11DeviceContext1>    m_d3dContext;
+
+	ComPtr<IDXGISwapChain1>         m_swapChain;
+	ComPtr<ID3D11RenderTargetView>  m_renderTargetView;
+	ComPtr<ID3D11DepthStencilView>  m_depthStencilView;
+
+	// Resource
+	ComPtr<ID3D11ShaderResourceView> m_texture;
+	std::unique_ptr<DirectX::SpriteBatch> m_spriteBatch;
+	DirectX::SimpleMath::Vector2 m_screenPos;
+	DirectX::SimpleMath::Vector2 m_origin;
+
+	// Rendering loop timer.
+	DX::StepTimer                                   m_timer;
+};
